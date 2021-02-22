702e64f64c09153fd766f6e67d78aa49eb46b0d2
koti meesala <kotislr75@gmail.com>
2021-02-22 01:47:01 -0600

    Add project files.

---
+++ MidTerm/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace MidTerm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.row1col1 = new System.Windows.Forms.Button();
+            this.row2col1 = new System.Windows.Forms.Button();
+            this.row2col2 = new System.Windows.Forms.Button();
+            this.row1col2 = new System.Windows.Forms.Button();
+            this.row1col3 = new System.Windows.Forms.Button();
+            this.row2col3 = new System.Windows.Forms.Button();
+            this.row3col1 = new System.Windows.Forms.Button();
+            this.row3col2 = new System.Windows.Forms.Button();
+            this.row3col3 = new System.Windows.Forms.Button();
+            this.row1sum = new System.Windows.Forms.Label();
+            this.row2sum = new System.Windows.Forms.Label();
+            this.row3sum = new System.Windows.Forms.Label();
+            this.col1sum = new System.Windows.Forms.Label();
+            this.col2sum = new System.Windows.Forms.Label();
+            this.col3sum = new System.Windows.Forms.Label();
+            this.topleftdiagonal = new System.Windows.Forms.Label();
+            this.bottomleftdiagonal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // row1col1
+            // 
+            this.row1col1.AllowDrop = true;
+            this.row1col1.Location = new System.Drawing.Point(102, 79);
+            this.row1col1.Name = "row1col1";
+            this.row1col1.Size = new System.Drawing.Size(86, 80);
+            this.row1col1.TabIndex = 0;
+            this.row1col1.Text = "1";
+            this.row1col1.UseVisualStyleBackColor = true;
+            this.row1col1.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDropHandler);
+            this.row1col1.DragEnter += new System.Windows.Forms.DragEventHandler(this.DragEnterHandler);
+            this.row1col1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownHandler);
+            // 
+            // row2col1
+            // 
+            this.row2col1.AllowDrop = true;
+            this.row2col1.Location = new System.Drawing.Point(102, 176);
+            this.row2col1.Name = "row2col1";
+            this.row2col1.Size = new System.Drawing.Size(86, 80);
+            this.row2col1.TabIndex = 0;
+            this.row2col1.Text = "4";
+            this.row2col1.UseVisualStyleBackColor = true;
+            this.row2col1.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDropHandler);
+            this.row2col1.DragEnter += new System.Windows.Forms.DragEventHandler(this.DragEnterHandler);
+            this.row2col1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownHandler);
+            // 
+            // row2col2
+            // 
+            this.row2col2.AllowDrop = true;
+            this.row2col2.Location = new System.Drawing.Point(217, 177);
+            this.row2col2.Name = "row2col2";
+            this.row2col2.Size = new System.Drawing.Size(86, 80);
+            this.row2col2.TabIndex = 1;
+            this.row2col2.Text = "5";
+            this.row2col2.UseVisualStyleBackColor = true;
+            this.row2col2.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDropHandler);
+            this.row2col2.DragEnter += new System.Windows.Forms.DragEventHandler(this.DragEnterHandler);
+            this.row2col2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownHandler);
+            // 
+            // row1col2
+            // 
+            this.row1col2.AllowDrop = true;
+            this.row1col2.Location = new System.Drawing.Point(217, 79);
+            this.row1col2.Name = "row1col2";
+            this.row1col2.Size = new System.Drawing.Size(86, 80);
+            this.row1col2.TabIndex = 2;
+            this.row1col2.Text = "2";
+            this.row1col2.UseVisualStyleBackColor = true;
+            this.row1col2.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDropHandler);
+            this.row1col2.DragEnter += new System.Windows.Forms.DragEventHandler(this.DragEnterHandler);
+            this.row1col2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownHandler);
+            // 
+            // row1col3
+            // 
+            this.row1col3.AllowDrop = true;
+            this.row1col3.Location = new System.Drawing.Point(330, 79);
+            this.row1col3.Name = "row1col3";
+            this.row1col3.Size = new System.Drawing.Size(86, 80);
+            this.row1col3.TabIndex = 3;
+            this.row1col3.Text = "3";
+            this.row1col3.UseVisualStyleBackColor = true;
+            this.row1col3.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDropHandler);
+            this.row1col3.DragEnter += new System.Windows.Forms.DragEventHandler(this.DragEnterHandler);
+            this.row1col3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownHandler);
+            // 
+            // row2col3
+            // 
+            this.row2col3.AllowDrop = true;
+            this.row2col3.Location = new System.Drawing.Point(331, 177);
+            this.row2col3.Name = "row2col3";
+            this.row2col3.Size = new System.Drawing.Size(86, 80);
+            this.row2col3.TabIndex = 4;
+            this.row2col3.Text = "6";
+            this.row2col3.UseVisualStyleBackColor = true;
+            this.row2col3.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDropHandler);
+            this.row2col3.DragEnter += new System.Windows.Forms.DragEventHandler(this.DragEnterHandler);
+            this.row2col3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownHandler);
+            // 
+            // row3col1
+            // 
+            this.row3col1.AllowDrop = true;
+            this.row3col1.Location = new System.Drawing.Point(102, 272);
+            this.row3col1.Name = "row3col1";
+            this.row3col1.Size = new System.Drawing.Size(86, 80);
+            this.row3col1.TabIndex = 5;
+            this.row3col1.Text = "7";
+            this.row3col1.UseVisualStyleBackColor = true;
+            this.row3col1.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDropHandler);
+            this.row3col1.DragEnter += new System.Windows.Forms.DragEventHandler(this.DragEnterHandler);
+            this.row3col1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownHandler);
+            // 
+            // row3col2
+            // 
+            this.row3col2.AllowDrop = true;
+            this.row3col2.Location = new System.Drawing.Point(217, 272);
+            this.row3col2.Name = "row3col2";
+            this.row3col2.Size = new System.Drawing.Size(86, 80);
+            this.row3col2.TabIndex = 6;
+            this.row3col2.Text = "8";
+            this.row3col2.UseVisualStyleBackColor = true;
+            this.row3col2.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDropHandler);
+            this.row3col2.DragEnter += new System.Windows.Forms.DragEventHandler(this.DragEnterHandler);
+            this.row3col2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownHandler);
+            // 
+            // row3col3
+            // 
+            this.row3col3.AllowDrop = true;
+            this.row3col3.Location = new System.Drawing.Point(333, 271);
+            this.row3col3.Name = "row3col3";
+            this.row3col3.Size = new System.Drawing.Size(86, 80);
+            this.row3col3.TabIndex = 7;
+            this.row3col3.Text = "9";
+            this.row3col3.UseVisualStyleBackColor = true;
+            this.row3col3.DragDrop += new System.Windows.Forms.DragEventHandler(this.DragDropHandler);
+            this.row3col3.DragEnter += new System.Windows.Forms.DragEventHandler(this.DragEnterHandler);
+            this.row3col3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MouseDownHandler);
+            // 
+            // row1sum
+            // 
+            this.row1sum.Location = new System.Drawing.Point(439, 111);
+            this.row1sum.Name = "row1sum";
+            this.row1sum.Size = new System.Drawing.Size(56, 32);
+            this.row1sum.TabIndex = 8;
+            this.row1sum.Text = "label1";
+            // 
+            // row2sum
+            // 
+            this.row2sum.Location = new System.Drawing.Point(439, 208);
+            this.row2sum.Name = "row2sum";
+            this.row2sum.Size = new System.Drawing.Size(56, 32);
+            this.row2sum.TabIndex = 9;
+            this.row2sum.Text = "label2";
+            // 
+            // row3sum
+            // 
+            this.row3sum.Location = new System.Drawing.Point(439, 303);
+            this.row3sum.Name = "row3sum";
+            this.row3sum.Size = new System.Drawing.Size(56, 32);
+            this.row3sum.TabIndex = 10;
+            this.row3sum.Text = "label3";
+            // 
+            // col1sum
+            // 
+            this.col1sum.Location = new System.Drawing.Point(99, 392);
+            this.col1sum.Name = "col1sum";
+            this.col1sum.Size = new System.Drawing.Size(56, 32);
+            this.col1sum.TabIndex = 11;
+            this.col1sum.Text = "label4";
+            // 
+            // col2sum
+            // 
+            this.col2sum.Location = new System.Drawing.Point(232, 392);
+            this.col2sum.Name = "col2sum";
+            this.col2sum.Size = new System.Drawing.Size(56, 32);
+            this.col2sum.TabIndex = 12;
+            this.col2sum.Text = "label5";
+            // 
+            // col3sum
+            // 
+            this.col3sum.Location = new System.Drawing.Point(344, 392);
+            this.col3sum.Name = "col3sum";
+            this.col3sum.Size = new System.Drawing.Size(56, 32);
+            this.col3sum.TabIndex = 13;
+            this.col3sum.Text = "label6";
+            // 
+            // topleftdiagonal
+            // 
+            this.topleftdiagonal.Location = new System.Drawing.Point(439, 392);
+            this.topleftdiagonal.Name = "topleftdiagonal";
+            this.topleftdiagonal.Size = new System.Drawing.Size(56, 32);
+            this.topleftdiagonal.TabIndex = 14;
+            this.topleftdiagonal.Text = "label7";
+            // 
+            // bottomleftdiagonal
+            // 
+            this.bottomleftdiagonal.Location = new System.Drawing.Point(439, 32);
+            this.bottomleftdiagonal.Name = "bottomleftdiagonal";
+            this.bottomleftdiagonal.Size = new System.Drawing.Size(56, 32);
+            this.bottomleftdiagonal.TabIndex = 15;
+            this.bottomleftdiagonal.Text = "label8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 508);
+            this.Controls.Add(this.bottomleftdiagonal);
+            this.Controls.Add(this.topleftdiagonal);
+            this.Controls.Add(this.col3sum);
+            this.Controls.Add(this.col2sum);
+            this.Controls.Add(this.col1sum);
+            this.Controls.Add(this.row3sum);
+            this.Controls.Add(this.row2sum);
+            this.Controls.Add(this.row1sum);
+            this.Controls.Add(this.row3col3);
+            this.Controls.Add(this.row3col2);
+            this.Controls.Add(this.row3col1);
+            this.Controls.Add(this.row2col3);
+            this.Controls.Add(this.row1col3);
+            this.Controls.Add(this.row1col2);
+            this.Controls.Add(this.row2col2);
+            this.Controls.Add(this.row2col1);
+            this.Controls.Add(this.row1col1);
+            this.Name = "Form1";
+            this.Text = "Magic Square 3x3";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button row1col1;
+        private System.Windows.Forms.Button row2col1;
+        private System.Windows.Forms.Button row2col2;
+        private System.Windows.Forms.Button row1col2;
+        private System.Windows.Forms.Button row1col3;
+        private System.Windows.Forms.Button row2col3;
+        private System.Windows.Forms.Button row3col1;
+        private System.Windows.Forms.Button row3col2;
+        private System.Windows.Forms.Button row3col3;
+        private System.Windows.Forms.Label row1sum;
+        private System.Windows.Forms.Label row2sum;
+        private System.Windows.Forms.Label row3sum;
+        private System.Windows.Forms.Label col1sum;
+        private System.Windows.Forms.Label col2sum;
+        private System.Windows.Forms.Label col3sum;
+        private System.Windows.Forms.Label topleftdiagonal;
+        private System.Windows.Forms.Label bottomleftdiagonal;
+    }
+}
+
---
+++ MidTerm/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MidTerm
+{
+    public partial class Form1 : Form
+    {
+        Button buttonBeingDragged;
+        public Form1()
+        {
+            InitializeComponent();
+            ComputeSumOfAllLabels();
+        }
+
+
+        private void ComputeSumOfAllLabels()
+        {
+            CalculateSumOfRow1();
+            CalculateSumOfRow2();
+            CalculateSumOfRow3();
+            CalculateSumOfCol1();
+            CalculateSumOfCol2();
+            CalculateSumOfCol3();
+            CalculateSumOfTopleftdiagonal();
+            CalculateSumOfBottomLeftDiagonal();
+        }
+
+        #region sum computation
+        private void CalculateSumOfRow1()
+        {
+            row1sum.Text = Convert.ToString(Convert.ToInt32(row1col1.Text) + Convert.ToInt32(row1col2.Text) +
+                Convert.ToInt32(row1col3.Text));
+        }
+
+        private void CalculateSumOfRow2()
+        {
+            row2sum.Text = Convert.ToString(Convert.ToInt32(row2col1.Text) + Convert.ToInt32(row2col2.Text) +
+                Convert.ToInt32(row2col3.Text));
+        }
+
+        private void CalculateSumOfRow3()
+        {
+            row3sum.Text = Convert.ToString(Convert.ToInt32(row3col1.Text) + Convert.ToInt32(row3col2.Text) +
+                Convert.ToInt32(row3col3.Text));
+        }
+
+        private void CalculateSumOfCol1()
+        {
+            col1sum.Text = Convert.ToString(Convert.ToInt32(row1col1.Text) + Convert.ToInt32(row2col1.Text) +
+                Convert.ToInt32(row3col1.Text));
+        }
+
+        private void CalculateSumOfCol2()
+        {
+            col2sum.Text = Convert.ToString(Convert.ToInt32(row1col2.Text) + Convert.ToInt32(row2col2.Text) +
+                Convert.ToInt32(row3col2.Text));
+        }
+
+        private void CalculateSumOfCol3()
+        {
+            col3sum.Text = Convert.ToString(Convert.ToInt32(row1col3.Text) + Convert.ToInt32(row2col3.Text) +
+                Convert.ToInt32(row3col3.Text));
+        }
+
+        private void CalculateSumOfTopleftdiagonal()
+        {
+            topleftdiagonal.Text = Convert.ToString(Convert.ToInt32(row1col1.Text) + Convert.ToInt32(row2col2.Text) +
+                Convert.ToInt32(row3col3.Text));
+        }
+
+        private void CalculateSumOfBottomLeftDiagonal()
+        {
+            bottomleftdiagonal.Text = Convert.ToString(Convert.ToInt32(row3col1.Text) + Convert.ToInt32(row2col2.Text) +
+                Convert.ToInt32(row1col3.Text));
+        }
+        #endregion
+
+        #region MouseDown Handlers
+        private void MouseDownHandler(object sender, MouseEventArgs e)
+        {
+            Button button = sender as Button;
+            buttonBeingDragged = button;
+            button.DoDragDrop(button.Text, DragDropEffects.Copy);
+        }
+
+        #endregion
+
+
+        #region DragEnter Handler
+        private void DragEnterHandler(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+        #endregion
+
+
+        #region DragDrop Handler
+        private void DragDropHandler(object sender, DragEventArgs e)
+        {
+            Button button = sender as Button;
+            buttonBeingDragged.Text = button.Text;
+            button.Text = e.Data.GetData(DataFormats.Text).ToString();
+            ComputeSumOfAllLabels();
+        }
+      
+        #endregion
+    }
+}
